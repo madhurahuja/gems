6030ee317f7cb81bc4d4d1b842f965d9bc57ab90
Madhur Ahuja <nicemadhur@gmail.com>
2020-08-29 16:37:20 -0500

    Moving settings to Configuration

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -4,13 +4,14 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtPayload } from './jwt-payload.interface';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as config from 'config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(@InjectRepository(UserRepository) private userRepository: UserRepository) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'weatherisawesome',
+      secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret'),
     });
   }
 
